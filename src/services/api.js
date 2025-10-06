3d71e3b2d4834f672af5b789abb782fa5962f6e7
SleepiestFool01 <teaganm856@gmail.com>
2025-10-06 16:18:53 -0500

    chldkfjnsl

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,8 @@
 import axios from "axios";
 
-// Define the base URL for the backend API
+//q
 // Use Vite env variable if present, otherwise fallback to localhost backend
-const base = "";
-if (import.meta.env.API) {
-  base = "http://localhost:3019/course-t9"
-} else{
-  base= "/course-t9"
-}
-
+const base = "http://localhost:3019/course-t9";
 
 // configure axios instance
 const api = axios.create({
