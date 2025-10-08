8555e9cb7aed72ce6e5ce64ee2cebc86eeb44cfd
parkerlandes <parker.landes@eagles.oc.edu>
2025-10-08 13:15:20 -0500

    delete import of style.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.js";
 import vuetify from "./plugins/vuetify.js";
-import './assets/style.css';
 
 createApp(App).use(vuetify).use(router).mount("#app");
